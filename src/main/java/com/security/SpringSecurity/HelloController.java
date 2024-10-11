5981124a394c2e6455b389f205e7dcd2b09cd82d
kutkut23 <sushma.1si14ec109@gmail.com>
2024-10-12 01:33:03 +0530

    Printing session Id

--- src/main/java/com/security/SpringSecurity/HelloController.java
+++ src/main/java/com/security/SpringSecurity/HelloController.java
@@ -1,13 +1,17 @@
 package com.security.SpringSecurity;
 
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 public class HelloController {
 
     @GetMapping("/")
-    public String hello() {
-        return "Hello World";
+    public String hello(HttpServletRequest request) {
+        log.info("Printing Session Id");
+        return "Hello World" + request.getSession().getId();
     }
 }
